06a1f902100098ad8853b3e9223e87b1cfe26662
Micah Switzer <micah.switzer.dev@outlook.com>
2018-10-08 09:11:00 -0400

    Added test project

---
+++ UDR.Game.Tests/GameTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace UDR.Game.Tests
+{
+    public class GameTest
+    {
+        [Fact]
+        public void Init()
+        {
+            
+        }
+    }
+}
